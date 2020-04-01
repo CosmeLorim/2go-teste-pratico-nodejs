d9bdf2095eaa333fde34315120d58fb8d189fa35
Cosme Lorim <cosme.lorim@gmail.com>
2020-04-01 01:33:41 -0400

    Remove old routes

--- src/config/server.js
+++ src/config/server.js
@@ -5,11 +5,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const enterpriseRoutes = require('../routes/v1/restricted/enterprise')
 const authenticationRoutes = require('../routes/v1/auth')
 const userRoutes = require('../routes/v1/user')
 
-app.use(enterpriseRoutes)
 app.use(authenticationRoutes)
 app.use(userRoutes)
 
